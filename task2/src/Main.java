f0ace502874a33a1724b2d7234c6f0239075314e
Tachenk0 <gorichenko.maks@mail.ru>
2026-03-02 15:56:15 +1000

    add sec lab

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -1,2 +1,15 @@
-public class Main{
+public class Main {
+
+    public static void main(String[] args) {
+
+        my_time t1 = new my_time();
+        my_time t2 = new my_time(555550000);
+        my_time t3 = new my_time(5, 23, 55);
+
+        System.out.println(t1);
+        System.out.println(t2);
+        System.out.println(t3);
+
+    }
+
 }
--- task2/src/my_time.java
+++ task2/src/my_time.java
@@ -1,3 +1,44 @@
 public class my_time {
-    public long hour,minute,second;
+
+    private long second = -1;
+    private long minute = -1;
+    private long hour = -1;
+
+    public my_time() {
+        long time = System.currentTimeMillis();
+        setTime(time);
+    }
+
+    public my_time(long time) {
+        setTime(time);
+    }
+
+    public my_time(long _hour, long _minute, long _second) {
+        second = _second;
+        minute = _minute;
+        hour = _hour;
+    }
+
+    public void setTime(long elapseTime) {
+        second = (elapseTime / 1000) % 60;
+        minute = (elapseTime / 60000) % 60;
+        hour = (elapseTime / 3600000) % 24;
+    }
+
+    public long getSeconds() {
+        return second;
+    }
+
+    public long getMinutes() {
+        return minute;
+    }
+
+    public long getHours() {
+        return hour;
+    }
+
+    public String toString() {
+        return getHours() + ":" + getMinutes() + ":" + getSeconds();
+    }
+
 }
