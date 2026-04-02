24fb5e40a4b19a9928f3c8afb5b97c3e7328c775
Tachenk0 <gorichenko.maks@mail.ru>
2026-04-02 11:13:00 +1000

    add 7 lab

---
+++ task 7/task-7/src/genericstack.java
@@ -0,0 +1,79 @@
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class genericstack<E> {
+    private int top;
+    private static final int DEFAULT_CAPACITY = 10;
+    private E[] arr;
+
+    public genericstack() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    public genericstack(int capacity) {
+        @SuppressWarnings("unchecked")
+        E[] temp = (E[]) new Object[capacity];
+        arr = temp;
+        top = -1;
+    }
+
+    public void push(E item) {
+        if(isFull()) {
+            resize(2 * arr.length);// х2 size
+        }
+
+        arr[++top] = item;
+    }
+
+    public E pop() {
+        if(isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        E item = arr[top];
+        arr[top--] = null;
+
+        if(top > -1 && top == arr.length / 4) {
+            resize(arr.length / 2);
+        }
+
+        return item;
+    }
+
+    public E peek() {
+        if(isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        return arr[top];
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return (top + 1) == arr.length;
+    }
+
+    public int size() {
+        return top + 1;
+    }
+
+    private void resize(int capacity) {
+        @SuppressWarnings("unchecked")
+
+        E[] temp = (E[]) new Object[capacity];
+        for(int i = 0; i <= top; i++) {
+            temp[i] = arr[i];
+        }
+
+        arr = temp;
+    }
+
+    @Override
+    public String toString() {
+        Object[] elements = Arrays.copyOf(arr, top + 1);
+        return "Массив: " + Arrays.toString(elements) + " [" + (isEmpty() ? "пуст" : "не пуст") + ", " + (isFull() ? "полный" : "не полный") + ", " + size() + ", " + arr.length + "]";
+    }
+}
--- task 7/task-7/src/newclass.java
+++ task 7/task-7/src/newclass.java
@@ -1,2 +1,37 @@
 public class newclass {
+
+    public static void main(String[] args) {
+
+        genericstack<String> arrStr = new genericstack<>(5);
+
+        System.out.println(arrStr); // empty
+
+        arrStr.push("Hi");
+        arrStr.push("Egor");
+
+        System.out.println(arrStr); // array with 2 of 5
+
+        arrStr.push("17");
+        arrStr.push("3");
+        arrStr.push("19");
+
+        System.out.println(arrStr); // max size
+
+        arrStr.push("5");
+        arrStr.push("10");
+
+        System.out.println(arrStr);// х2 size 7 of 10
+
+        arrStr.push("12");
+        arrStr.push("15");
+        arrStr.push("20");
+
+        System.out.println(arrStr); // max size
+
+        for(int i = 0; i < 5; i++) arrStr.pop();
+
+        System.out.println(arrStr);
+
+    }
+
 }
